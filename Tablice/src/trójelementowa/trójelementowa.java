47450b5af3c4d8a73d4877ae92c37a63ccf7def8
Bauaser <62292070+Bauaser@users.noreply.github.com>
2020-03-27 16:37:56 +0100

    Update trójelementowa.java

--- Tablice/src/trójelementowa/trójelementowa.java
+++ Tablice/src/trójelementowa/trójelementowa.java
@@ -12,14 +12,14 @@ public class trójelementowa {public static class Triple {
         }
         return greatest;
     }
-    public int greatestWithoutIf(int[] numbers) {
+    public int greatest2(int[] numbers) {
         return Math.max(numbers[0], Math.max(numbers[1], numbers[2]));
     }
     public static void main(String[] args) {
         Triple sample = new Triple();
-        int[] numbers = new int[]{1, 3, -5};
+        int[] numbers = new int[]{0, 7, -1};
         System.out.println(sample.Greatest(numbers));
-        System.out.println(sample.greatestWithoutIf(numbers));
+        System.out.println(sample.greatest2(numbers));
     }
 }
 }
